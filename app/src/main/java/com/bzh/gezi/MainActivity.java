e4a47081014c2ba87be551a3d95cb7cb8b156a35
biezhihua <448326497@qq.com>
2016-09-10 19:01:17 +0800

    updaet

--- app/src/main/java/com/bzh/gezi/MainActivity.java
+++ app/src/main/java/com/bzh/gezi/MainActivity.java
@@ -1,7 +1,17 @@
 package com.bzh.gezi;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 /**
  * ==========================================================<br>
@@ -39,9 +49,203 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayoutManager linearLayoutManager;
+    private boolean reverse = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = ((RecyclerView) findViewById(R.id.recycler));
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        final FibonacciDataSource fibonacciDataSource = new FibonacciDataSource();
+        final MyAdapter adapter = new MyAdapter(fibonacciDataSource.getDataSource());
+        recyclerView.setAdapter(adapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        fibonacciDataSource.addNewData(50);
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+                        super.onPostExecute(aVoid);
+                        adapter.notifyDataSetChanged();
+                    }
+                }.execute();
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
+
+    public void onReverse(View view) {
+        reverse = !reverse;
+        linearLayoutManager.setReverseLayout(reverse);
+    }
+
+    static class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+
+        private ArrayList<String> dataSource;
+
+        public MyAdapter(ArrayList<String> dataSource) {
+
+            this.dataSource = dataSource;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_content, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.content.setText("fib(" + position + ")=" + dataSource.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataSource.size();
+        }
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView content;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            content = (TextView) itemView.findViewById(R.id.content);
+        }
+    }
+
+    // 斐波那契数列数据源
+    // 利用额外的存储空间保存之前计算的结果,使新值计算的时间复杂度为BigInteger加法和乘法的复杂度 O(N)
+    static class FibonacciDataSource {
+
+        // 默认数据源数据量
+        private final int DEFAULT_DATA_SOURCE_SIZE = 50;
+
+        // 当前行数
+        private int current_row;
+
+        // 显示结果
+        private ArrayList<String> dataSource = new ArrayList<>();
+
+        // 主值列表
+        private ArrayList<BigInteger> fibList1 = new ArrayList<>();
+
+        // 副职列表
+        private ArrayList<BigInteger> fibList2 = new ArrayList<>();
+
+        private BigInteger fib_0 = new BigInteger("0");
+        private BigInteger fib_1 = new BigInteger("1");
+
+        // 1.0E10
+        private BigInteger threshold = new BigInteger("10000000000");
+
+        // 主值小系数
+        private BigInteger primary_small_coffcient = new BigInteger("2");
+        // 主值大系数
+        private BigInteger primary_large_coffcient = new BigInteger("3");
+
+        // 副值小系数
+        private BigInteger secondary_small_cofficent = new BigInteger("1");
+        // 副值大系数
+        private BigInteger secondary_large_cofficent = new BigInteger("2");
+
+        // 待加入值
+        private BigInteger primary = new BigInteger("0");
+        private BigInteger secondary = new BigInteger("0");
+
+        private BigInteger big_value_1 = new BigInteger("1");
+        private BigInteger big_value_4 = new BigInteger("4");
+
+        public FibonacciDataSource() {
+            for (int i = 1; i <= DEFAULT_DATA_SOURCE_SIZE; i++) {
+                current_row = i;
+                updateDataSource(current_row);
+            }
+        }
+
+        // 顺序添加count个新斐波那契数列值
+        public void addNewData(int count) {
+            int target = current_row + count;
+            for (int i = current_row; i <= target; i++) {
+                current_row = i;
+                updateDataSource(current_row);
+            }
+        }
+
+        // 根据前一行的斐波那契数列值计算当前行的斐波那契数列值
+        private void updateDataSource(int row) {
+
+            BigInteger to_be_calculated = new BigInteger(row * row + "");
+
+            if (to_be_calculated.compareTo(big_value_1) == 0) {
+                // 添加开始值
+                fibList1.add(big_value_1);
+                dataSource.add("1");
+                return;
+            } else {
+                if (to_be_calculated.compareTo(big_value_4) == 0) {
+                    // 计算起始副值
+                    secondary = secondary_small_cofficent.multiply(fib_0).add(secondary_large_cofficent.multiply(fib_1));
+                    fibList2.add(secondary);
+
+                    // 计算起始主值
+                    primary = primary_small_coffcient.multiply(fib_0).add(primary_large_coffcient.multiply(fib_1));
+                    fibList1.add(primary);
+                    dataSource.add(primary.toString());
+                } else {
+                    // 计算副值
+                    secondary = secondary_small_cofficent.multiply(fibList2.get(fibList2.size() - 1)).add(secondary_large_cofficent.multiply(fibList1.get(fibList1.size() - 1)));
+                    fibList2.add(secondary);
+
+                    // 计算主值
+                    primary = primary_small_coffcient.multiply(fibList2.get(fibList2.size() - 2)).add(primary_large_coffcient.multiply(fibList1.get(fibList1.size() - 1)));
+                    fibList1.add(primary);
+                    if (primary.compareTo(threshold) > 0) {
+
+                        String temp = primary.toString();
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(temp.substring(0, 1));
+                        sb.append(".");
+                        sb.append(temp.substring(1, 11));
+                        sb.append("E");
+                        sb.append(temp.length() - 1);
+
+                        dataSource.add(sb.toString());
+                    } else {
+                        dataSource.add(primary.toString());
+                    }
+                }
+            }
+
+            // 更新系数
+            BigInteger temp_secondary_large_cofficent = secondary_large_cofficent;
+            secondary_small_cofficent = secondary_small_cofficent.add(secondary_large_cofficent);
+            secondary_large_cofficent = secondary_small_cofficent.add(temp_secondary_large_cofficent);
+
+            // 更新系数
+            BigInteger temp_primary_large_coffcient = primary_large_coffcient;
+            primary_small_coffcient = primary_small_coffcient.add(primary_large_coffcient);
+            primary_large_coffcient = primary_small_coffcient.add(temp_primary_large_coffcient);
+        }
+
+        public ArrayList<String> getDataSource() {
+            return dataSource;
+        }
     }
 }
